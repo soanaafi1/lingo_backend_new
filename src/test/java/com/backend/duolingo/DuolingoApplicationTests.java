0cd00a1ebd72eb65e0b03ce9b71a923ffd7e862b
teddbug-00 <dicksonadanya269@gmail.com>
2025-05-31 14:01:55 +0000

    initialize duolingo project with Spring Boot and Maven setup

---
+++ src/test/java/com/backend/duolingo/DuolingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.duolingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuolingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
